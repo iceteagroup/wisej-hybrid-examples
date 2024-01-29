09b928cc6f29bf2bc7e963510462f861b60cf9d0
LRufenacht <llrufenacht@gmail.com>
2024-01-29 16:23:03 -0500

    DatabaseSynchronization example.

---
+++ DatabaseSynchronization/WisejWebPageApplication/DataHttpHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+
+namespace WisejWebPageApplication
+{
+	class DataHttpHandler : IHttpHandler
+	{
+		bool IHttpHandler.IsReusable => true;
+
+		void IHttpHandler.ProcessRequest(HttpContext context)
+		{
+			switch (context.Request.Path)
+			{
+				case "/getValue":
+					context.Response.Write(DataRepository.GetValue());
+					break;
+
+				case "/saveValue":
+					var value = context.Request.Form["value"];
+					context.Response.Write(DataRepository.SaveValue(value));
+					break;
+			}
+		}
+	}
+}
---
+++ DatabaseSynchronization/WisejWebPageApplication/Page1.cs
@@ -0,0 +1,17 @@
+﻿using Wisej.Web;
+
+namespace WisejWebPageApplication
+{
+	public partial class Page1 : Page
+	{
+		public Page1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, System.EventArgs e)
+		{
+			AlertBox.Show(DataRepository.GetValue());
+		}
+	}
+}
