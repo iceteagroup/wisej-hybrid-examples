658ec5841f672c5985a3c73d57ad4b11de0934f9
Levie Rufenacht <llrufenacht@gmail.com>
2024-06-26 11:01:39 -0400

    .

--- ExternalApps/HybridLocal/Page1.cs
+++ ExternalApps/HybridLocal/Page1.cs
@@ -13,24 +13,24 @@ namespace HybridLocal
 
 		private void buttonLyft_Click(object sender, System.EventArgs e)
 		{
-			if (Device.Launcher.CanOpen("lyft://ridetype?id=lyft_line"))
-				Device.Launcher.Open("lyft://ridetype?id=lyft_line");
+			Device.Launcher.Open("lyft://ridetype?id=lyft_line");
 		}
 
 		private void buttonMaps_Click(object sender, System.EventArgs e)
 		{
-			var url = "http://maps.google.com/?q=40.7128,-74.0060";
+			var coordinates = "40.726966,-74.006076";
 
-			if (Device.Info.System.Platform == DevicePlatform.iOS)
-				url = "maps://?q=40.7128,-74.0060";
+			var url = $"geo:{coordinates}";
+
+			if (Device.Info.System.IsApple)
+				url = $"maps://?q={coordinates}";
 
 			Device.Launcher.Open(url);
 		}
 
 		private void buttonFb_Click(object sender, System.EventArgs e)
 		{
-			if (Device.Launcher.CanOpen("fb://"))
-				Device.Launcher.Open("fb://");
+			Device.Launcher.Open("fb://");
 		}
 	}
 }
